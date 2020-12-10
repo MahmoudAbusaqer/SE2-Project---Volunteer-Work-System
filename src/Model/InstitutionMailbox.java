af5a82b139f8b4ccff3db6b4427ca50e7d7b430d
Mahmoud Abusaqer <Mahmoudabusaqer1@gmail.com>
2020-12-11 01:24:09 +0200

    I added the mailbox class in the Model

---
+++ src/Model/InstitutionMailbox.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mahmoud_Abusaqer
+ */
+public class InstitutionMailbox {
+    private int senderId;
+    private String senderName;
+    private String title;
+    private String body;
+    private Date date;
+
+    public InstitutionMailbox() {
+    }
+
+}
