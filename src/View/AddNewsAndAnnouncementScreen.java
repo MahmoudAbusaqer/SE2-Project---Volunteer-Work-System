3ad712c44e9763d0e68daacae9067258c531a211
amron2000 <amroncr7@gmail.com>
2020-12-16 00:09:19 +0200

    I added JavaFX Code to this Class

--- src/View/AddNewsAndAnnouncementScreen.java
+++ src/View/AddNewsAndAnnouncementScreen.java
@@ -9,6 +9,11 @@ import Controller.AddNewsAndAnnouncementManager;
 import Model.AddNewsAndAnnouncement;
 import java.util.Date;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 /**
  *
  * @author Mahmoud_Abusaqer
@@ -29,4 +34,61 @@ public class AddNewsAndAnnouncementScreen {
     public void AddNewsAndAnnouncement(int nOr, String title, String body, Date date) {
 
     }
-}
+
+        @FXML
+        private Pane rootpane;
+
+        @FXML
+        private Button ButtonMainPage;
+
+        @FXML
+        private Button ButtonAddNewsPage;
+
+        @FXML
+        private Button ButtonDOVMailBox;
+
+        @FXML
+        private Button ExitButton;
+
+        @FXML
+        private TextField TextFieldNOrA;
+
+        @FXML
+        private TextField TextFieldNewsTitle;
+
+        @FXML
+        private TextField TextFieldNewsDescription;
+
+        @FXML
+        private TextField TextFieldNewsDate;
+
+        @FXML
+        private Button ButtonSubmit;
+
+        @FXML
+        void ButtonExit(ActionEvent event) {
+
+        }
+
+        @FXML
+        void buttonApplyVolunteerPage(ActionEvent event) {
+
+        }
+
+        @FXML
+        void buttonMainPage(ActionEvent event) {
+
+        }
+
+        @FXML
+        void buttonStudentMailBox(ActionEvent event) {
+
+        }
+
+        @FXML
+        void buttonSubmit(ActionEvent event) {
+
+        }
+
+    }
+
