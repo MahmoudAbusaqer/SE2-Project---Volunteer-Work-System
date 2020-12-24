30e17a5304dc6ec2cefbfcad1ba30837296fad9e
amron2000 <amroncr7@gmail.com>
2020-12-24 23:51:51 +0200

    Startup Pages

---
+++ src/View/LoginPage.java
@@ -0,0 +1,24 @@
+package View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class LoginPage {
+
+    @FXML
+    private Pane grad;
+
+    @FXML
+    private Button buttonLogin;
+
+    @FXML
+    private PasswordField TextFieldPass;
+
+    @FXML
+    private TextField TextFieldUsername;
+
+}
+
---
+++ src/View/StartPage.java
@@ -0,0 +1,38 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class StartPage {
+
+    @FXML
+    private Pane rootPane;
+
+    @FXML
+    private Button InstitutionButton;
+
+    @FXML
+    private Button DOVButton;
+
+    @FXML
+    private Button StudentButton;
+
+    @FXML
+    void DOVButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void InstitutionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StudentButton(ActionEvent event) {
+
+    }
+
+}
+
