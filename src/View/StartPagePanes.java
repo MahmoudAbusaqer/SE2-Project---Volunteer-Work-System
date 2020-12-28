d5ae163d4bce927d70badac39bde8702f7be8549
amron2000 <amroncr7@gmail.com>
2020-12-28 19:25:57 +0200

    New Startup Page (2 panes)

---
+++ src/View/StartPagePanes.java
@@ -0,0 +1,62 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class StartPagePanes {
+
+    @FXML
+    private Pane rootPane;
+
+    @FXML
+    private Pane grad;
+
+    @FXML
+    private Button buttonLogin;
+
+    @FXML
+    private PasswordField TextFieldPass;
+
+    @FXML
+    private TextField TextFieldID;
+
+    @FXML
+    private Pane StartPagePane;
+
+    @FXML
+    private Pane grad1;
+
+    @FXML
+    private Button InstitutionButton;
+
+    @FXML
+    private Button DOVButton;
+
+    @FXML
+    private Button StudentButton;
+
+    @FXML
+    void DOVButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void InstitutionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StudentButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonLoginHandle(ActionEvent event) {
+
+    }
+
+}
